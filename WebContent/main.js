d167e72152e2b403a41d573524fe576c9c698107
angelSuho <angelsuho0303@gmail.com>
2024-07-23 20:28:07 +0900

    refactor: main page rendering

--- WebContent/main.js
+++ WebContent/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     // 모든 메뉴 데이터
     const menuData = {
-        '승차권': ['일반승차권 조회', '단체승차권 조회', '할인승차권 조회'],
+        '승차권예약': ['일반승차권 조회', '단체승차권 조회', '할인승차권 조회'],
         '관광열차': ['기차 여행 패키지', '레일 바이크', '테마 기차 여행'],
         '자유여행': ['자유 여행 패키지', '주말 여행 스페셜', '가족 여행 프로모션'],
         '여행상품': ['1박 2일 여행', '2박 3일 투어', '일일 투어'],
@@ -18,18 +18,23 @@ $(document).ready(function() {
 
     function showAllMenus() {
         dropdown.empty(); // 이전 내용 지우기
+    
+        const container = $('<div>').addClass('container d-flex justify-content-between'); // 새로운 컨테이너 div
+    
         Object.keys(menuData).forEach(key => {
             const section = $('<div>').addClass('menu-section');
-            const title = $('<h3>').text(key);
+            const title = $('<h4>').text(key);
             section.append(title);
-
+    
             menuData[key].forEach(item => {
                 const link = $('<a>').attr('href', '#').addClass('menu-link-sub').text(item);
                 section.append(link);
             });
-
-            dropdown.append(section);
+    
+            container.append(section); // 섹션을 컨테이너에 추가
         });
+    
+        dropdown.append(container); // 컨테이너를 드롭다운에 추가
         dropdown.show(); // 드롭다운 표시
     }
 
