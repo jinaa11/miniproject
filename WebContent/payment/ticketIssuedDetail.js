e427a679208d430e5f35da6e89f9ddbcbefff07c
jinaa11 <jinaa2047@gmail.com>
2024-07-25 15:20:09 +0900

    feat: 발권내역조회 페이지 구현

---
+++ WebContent/payment/ticketIssuedDetail.js
@@ -0,0 +1,34 @@
+// 발권 내역 조회
+$(function() {
+  const idKey = "USER-ID";
+  const loginInfo = localStorage.getItem(idKey);
+
+  console.log(loginInfo);
+  const defaultText = $('.customer-name').text();
+  console.log(defaultText);
+  $('.customer-name').text(loginInfo + ' ' + defaultText);
+  
+  $.getJSON("/WebContent/json/payment-info.json", function (data) {
+    $.each(data, function (index, item) {
+      let getTicket = "<tr>";
+      getTicket += '<td><input type="radio" name="select-ticket" value="' + index + '"></td>';
+      getTicket += '<td>편도</td>';
+      getTicket += '<td>' + item.travelInfo.date + '</td>';
+      getTicket += '<td>' + item.travelInfo.trainType + '<br>' + item.travelInfo.trainNumber + '</td>';
+      getTicket += '<td>' + item.travelInfo.departureStation + '<br>' + item.travelInfo.departureTime + '</td>';
+      getTicket += '<td>' + item.travelInfo.arrivalStation + '<br>' + item.travelInfo.arrivalTime + '</td>';
+      getTicket += '<td>59800원</td>';
+      getTicket += '<td>' + item.passengerInfo.length + '</td>';
+      getTicket += '<td>' + item.passengerInfo[index].seatType + '</td>';
+      getTicket += '<td>결제완료</td>';
+      getTicket += '<td>발권완료</td>';
+      getTicket += '<td><button class="refund-button btn btn-primary me-2">환불하기</button></td>'
+
+      $('#issued-ticket').append(getTicket);
+
+      $('.refund-button').on('click', function() {
+        window.location.href = '/WebContent/cancel/cancel.html';  
+      });
+    });
+  });
+});
