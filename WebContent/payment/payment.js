3042e1a5edc6dabb7c59e139ff1d703d331f5813
jinaa11 <jinaa2047@gmail.com>
2024-07-26 20:38:18 +0900

    style: 영수증, 발권내역 스타일 수정

--- WebContent/payment/payment.js
+++ WebContent/payment/payment.js
@@ -162,9 +162,6 @@ function handlePaymentCompletion() {
   $("#get-ticket-issued").click(function () {
     window.location.href = "ticketIssuedDetail.html";
   });
-  // $('#check-receipt').click(function() {
-  //   window.location.href = 'receipt.html';
-  // }); 
 }
 
 // 발권 내역 조회
@@ -204,49 +201,6 @@ async function getIssuedTickect(totalFare) {
 // 영수증 조회
 $(document).on('click', '#check-receipt', function() {
   console.log('aa');
+  // 새 창으로 띄우기
   var newWindow = window.open('receipt.html', 'ReceiptWindow', 'width=650,height=800');
-
-  // // 데이터 로드를 위해 새 창이 충분히 로드된 후 실행
-  // $(newWindow).on('load', function() {
-  //   $.getJSON("/WebContent/json/payment-info.json", function (data) {
-  //     console.log(data);
-  //     $.each(data, function (index, item) {
-  //       // 날짜
-  //       $(".receipt-item").eq(0).find(".item-value").text(item.travelInfo.date);
-  
-  //       // 열차 정보
-  //       const train = item.travelInfo.trainType + ' ' + item.travelInfo.trainNumber
-  //       + ' | ' + item.passengerInfo[index].seatType + ' | ' + item.passengerInfo[index].seatInfo + ' 외 3건';
-  //       $(".receipt-item").eq(1).find(".item-value").text(train);
-        
-  //       // 경로
-  //       const path = item.travelInfo.departureStation + ' ' + item.travelInfo.departureTime + ' > '
-  //                   + item.travelInfo.arrivalStation + ' ' + item.travelInfo.arrivalTime;
-  //       $(".receipt-item").eq(2).find(".item-value").text(path);
-  
-  //       // 승객 타입별 수
-  //       let adultCnt = 0;
-  //       let childCnt = 0;
-  
-  //       $.each(item.passengerInfo, function(i, passenger) {
-  //         if(passenger.passengerType === "어른") {
-  //           adultCnt++;
-  //         } else if(passenger.passengerType === '어린이') {
-  //           childCnt++;
-  //         }
-  //       });
-  //       $(".receipt-item").eq(3).find(".item-value").text('어른 ' + adultCnt + '매, 어린이 ' + childCnt + '매');
-  //       // 결제방식
-  //       $(".receipt-item").eq(4).find(".item-value").text(item.paymentInfo.paymentMethod);
-  //       // 승인일자
-  //       $(".receipt-item").eq(5).find(".item-value").text(item.paymentInfo.approvalDate);
-  //       // 승인번호
-  //       $(".receipt-item").eq(6).find(".item-value").text(item.paymentInfo.approvalNumber);
-  //       // 결제금액
-  //       $(".receipt-item").eq(7).find(".item-value").text(parseInt(item.paymentInfo.amount).toLocaleString('ko-KR') + '원');
-  //       // 총 영수금액
-  //       $(".receipt-item").eq(8).find(".item-value").text(parseInt(item.paymentInfo.amount).toLocaleString('ko-KR') + '원');
-  //     });
-  //   });
-  // });
 });
