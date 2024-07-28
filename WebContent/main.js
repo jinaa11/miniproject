c3a7e796c3a67c6cd2c85253008cddcc42864d47
ejuun <wndjs8225@gmail.com>
2024-07-29 01:47:12 +0900

    패키지 추가

--- WebContent/main.js
+++ WebContent/main.js
@@ -67,4 +67,60 @@ $(document).ready(function() {
     newdate.setDate(newdate.getDate() + 1);
     let new_date = newdate.toISOString().substring(0, 10);
     $('#date1').val(new_date);
-});
+
+
+
+    // 경상권 버튼 클릭 시 모달 열기
+  $('.btn-container a').on('click', function(e) {
+    e.preventDefault();
+    if ($(this).find('.btn-text').text() === '경상권') {
+        // JSON 데이터를 가져와서 리스트에 바인딩
+        $.getJSON('/WebContent/json/package.json', function(data) {
+            var packageList = $('#packageList');
+          packageList.empty(); // 기존 내용 삭제
+          $.each(data, function(index, item) {
+
+              var listItem = `
+                <a href="#" class="list-group-item list-group-item-action">
+                  <div class="d-flex w-100">
+                    <img src="${item.image}" class="img-thumbnail" alt="${item.title}">
+                    <small id="Map" style="display:none;">${item.map}</small>
+                    <small id="main" style="display:none;">${item.image}</small>
+                    <div class="ms-3">
+                      <h5 class="mb-1">${item.city} : ${item.title}</h5>
+                      <p class="mb-1">${item.content}</p>
+                      <strong>${item.pay}</strong>
+                    </div>
+                  </div>
+                </a>`;
+              packageList.append(listItem);
+          });
+          $('#packageModal').modal('show');
+        // 리스트 항목에 hover 이벤트 추가
+        $('.list-group-item').hover(function() {
+            var title = $(this).find('h5').text();
+            var content = $(this).find('p').text();
+            var pay = $(this).find('strong').text();
+            var image = $(this).find('#Map').text();
+            var mainiImage = $(this).find('#main').text();
+            var packageInfo = `
+
+                <img id="MainMap" src="${image}" alt="${title}">
+                <div style="height : 400px">
+                <img id="MainImg" src="${mainiImage}" alt="${title}">
+                <br><br>
+                <h1 style="font-size:2rem; font-weight: 'bold'">${title}</h1>
+                <br><br>
+                <h4 style="font-size:1.5rem">요금: ${pay}</h4>
+                </div>
+            `;
+            $('#packageMap').empty();
+            $('#packageMap').append(packageInfo);
+          });
+        $('.list-group-item').click(function() {
+            window.location.href = '/WebContent/trainSelect/train.html';
+          });
+        });
+      }
+    });
+  });
